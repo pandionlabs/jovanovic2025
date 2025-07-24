136f5177aa061d3a2f2058384b63a4be46c92107
Simone Massaro <simone.massaro@mone27.net>
2025-07-24 10:12:29 +0200

    removed warnings in positorn in tests

--- tests/testthat/test-clean_data.R
+++ tests/testthat/test-clean_data.R
@@ -1,6 +1,6 @@
 
-TreMs <- clean_data(jovanovic2025::MasterThesisData2024)
-test_that("clean_data works", {
+TreMs <- clean_data(MasterThesisData2024)
+testthat::test_that("clean_data works", {
   expect_length(TreMs, 101)
   expect_equal(nrow(TreMs), 533)
 })
